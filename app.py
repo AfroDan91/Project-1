eb1ee523e174cee7ea01fa49b5b442d3cd11be9d
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-07-12 17:48:54 +0000

    started my app.py coding. basic outline of pages started

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def wellcome_message():
-    return redirect(url_for('home_page'))
+def main_page():
+    pass
 
 @app.route('/home')
-def home_page():
-    return "Hello and welcome to my app"
+def home():
+    pass
 
-        
+@app.route('/employees')
+def emp():
+    pass
 
+app.route('/department')
+def dep():
+    pass
+
+app.route('/about')
+def about():
+    pass
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
