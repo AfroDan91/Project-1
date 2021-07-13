a374b8741dcf6e92814a37b6c551ef5d63b4a8a6
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-07-13 18:04:30 +0000

    using what I learned today I have made a better start on the db and webpage functionality

---
+++ application/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('databse_uri'))
+
+db = SQLAlchemy(app)
+
+from application import routes
